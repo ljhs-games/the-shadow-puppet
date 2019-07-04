9cacb54bfac3b7a381d33bd69d9457f0c6d40994
Cameron Reikes <cameronreikes@gmail.com>
2019-07-03 17:41:26 -0700

    Add basic lightning scene

--- src/TitleScreenCanvasLayer.gd
+++ src/TitleScreenCanvasLayer.gd
@@ -6,3 +6,9 @@ func _on_CreditsButton_pressed():
 
 func _on_BasicButton_pressed():
 	$CreditsSwitcher.play("ToTitleScreen")
+
+func _on_PlayButton_pressed():
+	get_node("/root/TitleScreen/TitleScreenAnimation").play("FadeOut")
+
+func goto_game_scene():
+	get_tree().change_scene("res://Game.tscn")
