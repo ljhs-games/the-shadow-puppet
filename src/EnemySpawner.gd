07756710cc9a23029a60bec861c3f8e19ef21e26
Cameron Reikes <cameronreikes@gmail.com>
2019-07-04 04:01:25 -0700

    Make shadow enemies attack player

--- src/EnemySpawner.gd
+++ src/EnemySpawner.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 export (NodePath) var dispenser_path
+export (NodePath) var player_path
 export (PackedScene) var shadow_enemy_pack
 
 onready var dispenser_node = get_node(dispenser_path)
+onready var player_node = get_node(player_path)
 
 var spawn_time = 3.0
 var spawn_decrease_per_minute = 1.0
@@ -27,6 +29,7 @@ func spawn_guy():
 	var cur_enemy = shadow_enemy_pack.instance()
 	add_child(cur_enemy)
 	cur_enemy.dispenser_coordinates = dispenser_node.global_position
+	cur_enemy.player_node = player_node
 	cur_enemy.global_position.x = randi()%2 * 600.0
 	cur_enemy.global_position.y = rand_range(0, 500.0)
 #	print("spawning!")
--- src/ShadowEnemy.gd
+++ src/ShadowEnemy.gd
@@ -4,15 +4,29 @@ export var damage_rate = 5.0
 export var move_speed = 100.0
 
 var dispenser_coordinates = Vector2(300.0, 0.0)
+var player_node = null
 var target_dissasembly = null
+var target_player = false
+var attacking_player = false
+
+func _ready():
+	randomize()
+	target_player = (randi()%2) as bool
 
 func _process(delta):
+	var target_coords = dispenser_coordinates
+	var closing_distance = 50.0
+	if target_player and player_node != null:
+		target_coords = player_node.global_position
+		closing_distance = 5.0
 	if target_dissasembly != null:
 		target_dissasembly.health -= damage_rate*delta
-	rotation = global_position.angle_to_point(dispenser_coordinates) + PI
-	if (dispenser_coordinates - global_position).length() <= 50.0:
+	rotation = global_position.angle_to_point(target_coords) + PI
+	if attacking_player and player_node != null:
+		player_node.health -= damage_rate*delta
+	if (target_coords - global_position).length() <= closing_distance:
 		return
-	global_position += (dispenser_coordinates - global_position).normalized()*move_speed*delta
+	global_position += (target_coords - global_position).normalized()*move_speed*delta
 
 func _on_ShadowEnemy_area_entered(area):
 	if area.is_in_group("dispenser"):
@@ -31,3 +45,19 @@ func _on_DismantlePlayer2D_finished():
 	if target_dissasembly != null and is_processing() == false:
 		$DismantlePlayer2D.pitch_scale = rand_range(0.9, 1.2)
 		$DismantlePlayer2D.play()
+
+func _on_ShadowEnemy_body_entered(body):
+	if body.is_in_group("player"):
+		attacking_player = true
+		$HurtPlayerPlayer2D.play()
+
+
+func _on_ShadowEnemy_body_exited(body):
+	if body.is_in_group("player"):
+		attacking_player = false
+		$HurtPlayerPlayer2D.playing = false
+
+func _on_HurtPlayerPlayer2D_finished():
+	if attacking_player:
+		$HurtPlayerPlayer2D.pitch_scale = rand_range(0.8, 1.3)
+		$HurtPlayerPlayer2D.play()
